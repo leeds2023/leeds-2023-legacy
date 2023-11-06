6457294790177b9d43e7ad6902dfc0561711778e
Sam Ducker <sam@luckyduck.digital>
2023-11-06 00:02:21 +0000

    Add theme, add core ui components, add hero/highlights scaffolded

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+/** @type {import("@types/eslint").Linter.Config */
 module.exports = {
   env: {
     node: true,
@@ -5,85 +6,85 @@ module.exports = {
     browser: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:astro/recommended",
-    "plugin:jsx-a11y/recommended", // Add the recommended accessibility rules
+    'eslint:recommended',
+    'plugin:astro/recommended',
+    'plugin:jsx-a11y/recommended', // Add the recommended accessibility rules
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {
-    "no-mixed-spaces-and-tabs": "off",
+    'no-mixed-spaces-and-tabs': 'off',
   },
   settings: {
     react: {
-      pragma: "h",
-      version: "detect", // Changed to 'detect' to auto-detect React version
+      pragma: 'h',
+      version: 'detect', // Changed to 'detect' to auto-detect React version
     },
   },
   plugins: [
     // Add 'jsx-a11y' to the plugins array
-    "jsx-a11y",
+    'jsx-a11y',
   ],
   overrides: [
     {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
       },
       rules: {},
     },
     {
-      files: ["*.ts"],
-      parser: "@typescript-eslint/parser",
-      extends: ["plugin:@typescript-eslint/recommended"],
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
       rules: {
-        "@typescript-eslint/no-unused-vars": [
-          "error",
-          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
         ],
-        "@typescript-eslint/no-non-null-assertion": "off",
+        '@typescript-eslint/no-non-null-assertion': 'off',
       },
     },
     {
-      files: ["*.d.ts"],
+      files: ['*.d.ts'],
       rules: {
-        "@typescript-eslint/triple-slash-reference": "off",
+        '@typescript-eslint/triple-slash-reference': 'off',
       },
     },
     {
-      files: ["*.tsx"],
-      parser: "@typescript-eslint/parser",
-      plugins: ["react", "@typescript-eslint", "jsx-a11y"], // Ensure 'jsx-a11y' is included for .tsx files
+      files: ['*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['react', '@typescript-eslint', 'jsx-a11y'], // Ensure 'jsx-a11y' is included for .tsx files
       parserOptions: {
         tsconfigRootDir: __dirname,
-        project: ["./tsconfig.json"],
+        project: ['./tsconfig.json'],
       },
       extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:jsx-a11y/recommended", // Ensure 'jsx-a11y' recommended rules are included for .tsx files
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:jsx-a11y/recommended', // Ensure 'jsx-a11y' recommended rules are included for .tsx files
       ],
       rules: {
-        "@typescript-eslint/no-unused-vars": [
-          "error",
-          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
         ],
-        "@typescript-eslint/no-non-null-assertion": "off",
+        '@typescript-eslint/no-non-null-assertion': 'off',
         // You can override or add additional accessibility rules here
       },
     },
     {
       // Define the configuration for `<script>` tag.
       // Script in `<script>` is assigned a virtual file name with the `.js` extension.
-      files: ["**/*.astro/*.js", "*.astro/*.js"],
-      parser: "@typescript-eslint/parser",
+      files: ['**/*.astro/*.js', '*.astro/*.js'],
+      parser: '@typescript-eslint/parser',
     },
   ],
 };
