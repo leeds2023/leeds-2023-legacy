d1c82eb3334140fcd8117c1f0cdcde3845e6038a
Sam Ducker <sam@luckyduck.digital>
2023-12-16 20:45:28 +0000

    Add storyblok link parser

--- src/lib/storyblok/helpers.ts
+++ src/lib/storyblok/helpers.ts
@@ -4,6 +4,7 @@ import type {
 	StoryWithNavAndFooter,
 	StoryWithLegacyPage,
 	LegacyPageStories,
+	Link,
 } from './types';
 
 const storyblokApi = useStoryblokApi();
@@ -65,4 +66,53 @@ export async function fetchDataSources(dataSourceId: string): Promise<DataSource
 export async function fetchCategoryTags(): Promise<DataSourceResponse> {
 	const data = await fetchDataSources('category-tags');
 	return data;
+}
+
+function isValidUrl(link: Link) {
+	return !!(
+		link.url !== null ||
+		link.email !== null ||
+		link.cached_url !== null ||
+		isValidFullSlug(link)
+	);
+}
+
+function isValidFullSlug(link: Link) {
+	return typeof link === 'string' && link[0] !== '/';
+}
+
+export function parseStoryblokLink(link: Link): string {
+	const NULL_LINK = '#';
+
+	if (!isValidUrl(link)) {
+		return NULL_LINK;
+	}
+
+	if (link.story) {
+		if (link.anchor) {
+			return `/${link.story?.url ?? ''}#${link.anchor ?? ''}`.replaceAll('//', '/');
+		}
+		return `/${link.story?.url || ''}`.replaceAll('//', '/');
+	}
+
+	if (link.url) {
+		if (link.anchor !== null) {
+			return `${link.url}#${link.anchor ?? ''}`;
+		}
+		return link.url;
+	}
+
+	if (link.email) {
+		return `mailto:${link.email ?? ''}`;
+	}
+
+	if (link.cached_url) {
+		if (link.anchor) {
+			return `/${link.cached_url ?? ''}#${link.anchor ?? ''}`;
+		} else {
+			return `/${link.cached_url ?? ''}`;
+		}
+	}
+
+	return '/';
 }
