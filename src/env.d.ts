443e8a181069e75d16357e99e8c10bb663978a04
Sam Ducker <sam@luckyduck.digital>
2023-12-16 20:49:34 +0000

    add typed enviroment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	GREENSOCK_TOKEN: string;
+	STORYBLOK_TOKEN: string;
+	STORYBLOK_ENV: 'draft' | 'published';
+	OUTPUT_STRATEGY: 'hybrid' | 'static' | 'server';
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
