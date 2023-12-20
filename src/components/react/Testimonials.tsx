94b94adc2b8ad291e381d616531340d1fd9071ec
Sam Ducker <sam@luckyduck.digital>
2023-12-20 13:59:23 +0000

    Mobile updates to testimonials

--- src/components/react/Testimonials.tsx
+++ src/components/react/Testimonials.tsx
@@ -21,16 +21,12 @@ export default function Testimonials({ blok }: TestimonialsProps) {
 	}
 
 	function handleIncreaseSlide() {
-		console.log('hi');
 		if (slide === blok.item.length - 1) {
 			setSlide(0);
 			return;
 		}
 		setSlide(slide + 1);
 	}
-
-	console.log(blok.item);
-	console.log(slide);
 
 	return (
 		<div className="mx-auto flex flex-col items-center justify-center gap-4 bg-brandMint-100 p-12 text-center text-darkText-30 lg:w-fit">
@@ -46,17 +42,17 @@ export default function Testimonials({ blok }: TestimonialsProps) {
 					__html: renderRichText(blok.item[slide].description),
 				}}
 			/>
-			<div className="flex gap-5 pt-4">
+			<div className="flex gap-8 pt-4 lg:gap-5">
 				<button
 					onClick={handleDecreaseSlide}
-					className="flex items-center justify-center rounded-full border-2 border-darkText-30 p-3"
+					className="flex items-center justify-center rounded-full border-2 border-darkText-30 p-1.5 lg:p-3"
 				>
 					<span className="sr-only">Previous</span>
 					<img src="images/icons/arrow.svg" alt="" />
 				</button>
 				<button
 					onClick={handleIncreaseSlide}
-					className="flex items-center justify-center rounded-full border-2 border-darkText-30 p-3"
+					className="flex items-center justify-center rounded-full border-2 border-darkText-30 p-1.5 lg:p-3"
 				>
 					<span className="sr-only">Next</span>
 					<img src="images/icons/arrow.svg" alt="" className="rotate-180" />
