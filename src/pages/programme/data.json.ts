09568ecc7c297c34ea436364566402cd2ec84bc1
Sam Ducker <sam@luckyduck.digital>
2023-12-20 16:15:14 +0000

    Update data.json.ts

--- src/pages/programme/data.json.ts
+++ src/pages/programme/data.json.ts
@@ -1,7 +1,7 @@
 export const prerender = true;
 
-import { fetchAllProjects } from '@/lib/storyblok/helpers';
-import type { ProjectStoryblok, SectionBlock } from '@/lib/storyblok/types';
+import { fetchAllProjectsTransformed } from '@/lib/storyblok/helpers';
+import type { ProjectStoryblok } from '@/lib/storyblok/types';
 import type { APIRoute } from 'astro';
 
 export type Project = {
@@ -16,28 +16,9 @@ export type Project = {
 export type ProjectsApiResponse = Project[];
 
 export const GET: APIRoute = async () => {
-	const data = await fetchAllProjects();
-	const mappedData = data.map((project) => {
-		return {
-			uuid: project.uuid,
-			content:
-				project.content.blocks[0].blocks.find(
-					(blok: SectionBlock) => blok.component === 'project'
-				) ?? {},
-			slug: project.slug,
-			tags:
-				project.content.blocks[0].blocks.find((blok: SectionBlock) => blok.component === 'project')
-					.tags ?? {},
-			category:
-				project.content.blocks[0].blocks.find((blok: SectionBlock) => blok.component === 'project')
-					.associatedStage ?? '',
-			title:
-				project.content.blocks[0].blocks.find((blok: SectionBlock) => blok.component === 'project')
-					.title ?? '',
-		};
-	});
+	const data = await fetchAllProjectsTransformed();
 
-	return new Response(JSON.stringify(mappedData), {
+	return new Response(JSON.stringify(data), {
 		status: 200,
 		headers: {
 			'content-type': 'application/json',
